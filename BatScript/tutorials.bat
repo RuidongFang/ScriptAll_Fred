59c1ea8438bbab63c95cb8aa7e2e37d0eccf7a3c
Fred <Fred1300216021@126.com>
2017-06-24 10:48:06 +0800

    Create tutorials.bat

---
+++ BatScript/tutorials.bat
@@ -0,0 +1,18 @@
+::::::::::::::::::::::::::::::::::::::::::::::::
+:: BatScript Tutorials
+::::::::::::::::::::::::::::::::::::::::::::::::
+
+:: (1) rem/::/REM：注释
+echo rem/::/REM的功能是注释
+
+:: (2) echo off：表示在此语句后的所有命令都不显示命令本身
+echo echo off或者@echo off：表示在此语句后的所有命令都不显示命令本身
+echo off
+
+:: (3) echo：显示此命令后的字符
+echo Hello BatScript!
+
+:: (4) pause：暂停批处理的执行程序并在屏幕上显示Press any key to continue...的提示，等待用户按任意键后继续
+echo pause的功能是暂停批处理的执行程序并在屏幕上显示Press any key to continue...的提示，等待用户按任意键后继续
+pause
+
