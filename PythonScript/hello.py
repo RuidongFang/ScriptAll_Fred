000bfc348c704b4221800f0cf6f8d802fe298e10
Fred <Fred1300216021@126.com>
2017-06-24 14:14:15 +0800

    Update hello.py

--- PythonScript/hello.py
+++ PythonScript/hello.py
@@ -1,2 +1,4 @@
-if __name__ == "__main__":
+# -*- coding:utf-8 -*-          # 使用中文注释
+# !/usr/bin/python              # 指定解释器
+if __name__ == "__main__":      # main函数
   print "Hello PythonScript!"
