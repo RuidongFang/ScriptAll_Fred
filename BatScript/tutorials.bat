ba881895abaece3c37f68756c0567c3daf683f63
Fred <Fred1300216021@126.com>
2017-06-24 10:52:33 +0800

    Update tutorials.bat

--- BatScript/tutorials.bat
+++ BatScript/tutorials.bat
@@ -8,6 +8,7 @@ echo rem/::/REM的功能是注释
 :: (2) echo off：表示在此语句后的所有命令都不显示命令本身
 echo echo off或者@echo off：表示在此语句后的所有命令都不显示命令本身
 echo off
+:: 注意：@与echo off相象，但它是加在每个命令行的最前面，表示运行时不显示这一行的命令行（只能影响当前行）
 
 :: (3) echo：显示此命令后的字符
 echo Hello BatScript!
